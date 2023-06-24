7726bb171f74329e8121a820b7673448bf08f8b6
Fujikouki <c1202599@st.kanazawa-it.ac.jp>
2023-06-24 13:10:28 +0900

    サービス化起動確認

--- app/src/main/java/com/example/bluetoothbackground/MainActivity.kt
+++ app/src/main/java/com/example/bluetoothbackground/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.example.bluetoothbackground
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.widget.Button
+import android.widget.Switch
+import android.view.View
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val listener = ScanListener()
+
+        var scanButton = findViewById<Button>(R.id.button)
+        scanButton.setOnClickListener(listener)
+
+        var cancelButton = findViewById<Button>(R.id.button2)
+        cancelButton.setOnClickListener(listener)
+
     }
+
+    private inner class ScanListener : View.OnClickListener {
+        override fun onClick(view: View) {
+
+            when (view.id) {
+                R.id.button -> {
+                    val intent = Intent(this@MainActivity, bluetoothbk::class.java)
+
+                    startService(intent)
+
+
+                }
+
+                R.id.button2 -> {
+                    val intent = Intent(this@MainActivity, bluetoothbk::class.java)
+
+                    stopService(intent)
+
+                }
+            }
+
+
+        }
+    }
+
+
+
+
 }
+
---
+++ app/src/main/java/com/example/bluetoothbackground/bluetoothbk.kt
@@ -0,0 +1,108 @@
+package com.example.bluetoothbackground
+
+import android.app.Service
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothAdapter
+import android.content.BroadcastReceiver
+import android.content.Intent
+import android.os.IBinder
+import android.content.Context
+import android.util.Log
+import android.content.IntentFilter
+import android.location.LocationManager
+import android.provider.Settings
+import android.widget.Toast
+
+class bluetoothbk : Service() {
+
+    private val REQUEST_ENABLEBLUETOOTH: Int = 1
+    private val MY_REQUEST_CODE: Int = 2
+
+    private var bluetoothAdapter: BluetoothAdapter? = null
+
+    private var gpsEnabled: Boolean = false
+
+    var MacAddressSet = mutableSetOf<String?>()
+
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            //Log.d("startSuccess1", "onReceive() success")
+            val action: String? = intent.action
+            when(action) {
+                BluetoothDevice.ACTION_FOUND -> {
+
+                    val device: BluetoothDevice? =
+                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+
+                    if (device == null) {
+                        Log.d("nullDevice", "Device is null")
+                        return
+                    }
+
+                    val deviceHardwareAddress = device?.address // MAC address
+
+                    MacAddressSet.add(deviceHardwareAddress)
+                    Log.d("MacA", MacAddressSet.toString())
+                    return
+                }
+
+            }
+        }
+    }
+
+    override fun onCreate() {
+        Log.d("startSuccess2", "onCreate() success")
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if (bluetoothAdapter == null) {
+            Log.d("bluetoothAdapterTest", "null")
+            onDestroy()
+            return
+
+        }
+        var locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+
+        registerReceiver(receiver, IntentFilter(BluetoothDevice.ACTION_FOUND))
+
+        Log.d("startSuccess3", "onCreate() success")
+
+
+    }
+
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d("startSuccess4", "onStartCommand() success")
+
+
+
+        if(bluetoothAdapter!!.startDiscovery()){
+
+            Log.d("startSuccess5", "startDiscovery() success")
+
+        }
+        
+        startReceiver()
+
+
+        return START_NOT_STICKY
+
+    }
+
+    private fun startReceiver() {
+        val intent = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        registerReceiver(receiver, intent)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
+
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+
+}
